1c475ba038a455362ad6bad9a4abd99366f5f25f
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2026-04-16 13:31:23 -0300

    chore: config vitest

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+import swc from 'unplugin-swc'
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    alias: {
+      '@': './src',
+      '@src': './src',
+      '@test': './test',
+    },
+    root: './',
+  },
+  resolve: {
+    alias: {
+      '@': './src',
+      '@src': './src',
+      '@test': './test',
+    },
+  },
+  plugins: [swc.vite()],
+})
