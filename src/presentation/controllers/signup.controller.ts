ce5871401ddc1d0f76917519e399ebfb32c22373
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2026-04-16 15:49:06 -0300

    feat: signup controller deve retornar mensagem de erro se o email não for informado

--- src/presentation/controllers/signup.controller.ts
+++ src/presentation/controllers/signup.controller.ts
@@ -1,8 +1,20 @@
 export default class SignupController {
     handle(httpRequest: any): any {
-        return {
-            statusCode: 400,
-            body: new Error('Parâmetro "name" não informado')
+
+        if (!httpRequest.body.name) {
+            return {
+                statusCode: 400,
+                    body: new Error('Parâmetro "name" não informado')
+            }
         }
+
+        if (!httpRequest.body.email) {
+            return {
+                statusCode: 400,
+                body: new Error('Parâmetro "email" não informado')
+            }
+        }
+
+        return
     }
 }
