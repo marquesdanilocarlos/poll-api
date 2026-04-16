cba2f74580481cb60bdf1458fe667f5c3a2eb901
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2026-04-16 15:38:15 -0300

    test: signup controller deve retornar 400

---
+++ src/presentation/controllers/signup-controller.test.ts
@@ -0,0 +1,16 @@
+import SignupController from '@/presentation/controllers/signup.controller'
+
+describe('SignUp Controller', () => {
+    test('Deve retornar 400 se não for informado o nome', () => {
+        const sut = new SignupController()
+        const request = {
+            body: {
+                email: 'marquesdanilocarlos@gmail.com',
+                password: '123456',
+                confirmPassword: '123456'
+            }
+        }
+        const httpResponse = sut.handle(request)
+        expect(httpResponse.statusCode).toBe(400)
+    })
+})
