69c9c0c698c060845800b3a909686b5e5f0607f6
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2026-04-16 15:49:18 -0300

    test: signup controller deve retornar mensagem de erro se o email não for informado

--- src/presentation/controllers/signup-controller.test.ts
+++ src/presentation/controllers/signup-controller.test.ts
@@ -14,4 +14,18 @@ describe('SignUp Controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(Error ('Parâmetro "name" não informado'))
     })
+
+    test('Deve retornar 400 se não for informado o email', () => {
+        const sut = new SignupController()
+        const request = {
+            body: {
+                name: 'Danilo Marques',
+                password: '123456',
+                confirmPassword: '123456'
+            }
+        }
+        const httpResponse = sut.handle(request)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(Error ('Parâmetro "email" não informado'))
+    })
 })
