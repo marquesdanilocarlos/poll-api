f03e7e31183b894dc6a57fd3a6a138b2ba9de8af
Danilo Carlos Marques <marquesdanilocarlos@gmail.com>
2026-04-18 07:41:59 -0300

    test: retornar 200 no SignupController caso os dados sejam válidos

--- src/infra/controllers/signup/signup-controller.test.ts
+++ src/infra/controllers/signup/signup-controller.test.ts
@@ -202,4 +202,25 @@ describe('SignUp Controller', () => {
         expect(httpResponse.statusCode).toBe(500)
         expect(httpResponse.body).toEqual(new ServerError())
     })
+
+    test('Deve retornar 200 se dados válidos forem passados', () => {
+        const {sut} = makeSut()
+        const request = {
+            body: {
+                name: 'valid_name',
+                email: 'valid_email@mail.com',
+                password: 'valid_password',
+                confirmPassword: 'valid_password'
+            }
+        }
+
+        const httpResponse = sut.handle(request)
+        expect(httpResponse.statusCode).toBe(200)
+        expect(httpResponse.body).toEqual({
+            id: 'valid_id',
+            name: 'valid_name',
+            email: 'valid_email@mail.com',
+            password: 'valid_password'
+        })
+    })
 })
